fe2fc0920e172e69c368eea27dc9508b122b4d1d
James McGrath <jmcgrath@mchale.net>
2024-11-08 16:59:23 +0000

    Make smote and undersampling optional so can compare the effects of adjusting them Drop some columns with high correlation during feature selection

--- ml_ia.py
+++ ml_ia.py
@@ -18,7 +18,10 @@ from sklearn.metrics import confusion_matrix
 from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import SMOTE
 
-visualise = 1
+verbose = 1
+save_intermediate_files = 1
+use_smote = 1
+use_under_sampling = 1
 
 ###################################################################################
 # Data read, initial cleanup and merge for Peugeot journeys
@@ -27,12 +30,12 @@ visualise = 1
 # Read the CSV files in to data frames
 peugeot_01 = pd.read_csv("peugeot_207_01.csv", sep=";")
 
-if visualise == 1:
+if verbose == 1:
     print("\n\nPrinting Peugeot Journey 1 Info")
     print( peugeot_01.info() )
 
 peugeot_02 = pd.read_csv("peugeot_207_02.csv", sep=";")
-if visualise == 1:
+if verbose == 1:
     print("\n\nPrinting Peugeot Journey 2 Info")
     print( peugeot_02.info() )
 
@@ -42,7 +45,7 @@ peugeot = pd.concat([peugeot_01, peugeot_02])
 # Insert a new column brand to distinguish each car model when merge with Opel
 peugeot["brand"] = "peugeot"
 
-if visualise == 1:
+if verbose == 1:
     print("\n\nPrinting Peugeot Merged Journey Info")
     print( peugeot.info() )
 
@@ -69,7 +72,7 @@ print( peugeot.isnull().sum() )
 print('\n\nPrinting the sum of na values in peugeot data frame')
 print( peugeot.isna().sum() )
 
-if visualise == 1:
+if verbose == 1:
     print("\n\nPrinting the cleaned up peugeot data frame")
     print( peugeot.info() )
     peugeot.to_csv('IntermediateDataPlots/peugeot.csv', index=False)
@@ -80,13 +83,13 @@ if visualise == 1:
 
 opel_01 = pd.read_csv("opel_corsa_01.csv", sep=";")
 
-if visualise == 1:
+if verbose == 1:
     print("\n\nPrinting Opel Journey 1 Info")
     print( opel_01.info() )
 
 opel_02 = pd.read_csv("opel_corsa_02.csv", sep=";")
 
-if visualise == 1:
+if verbose == 1:
     print("\n\nPrinting Opel Journey 2 Info")
     print( opel_02.info() )
 
@@ -96,7 +99,7 @@ opel = pd.concat([opel_01, opel_02])
 # Insert a new column brand to distinguish from Peugeot
 opel["brand"] = "opel"
 
-if visualise == 1:
+if verbose == 1:
     print("\n\nPrinting Opel Merged Journey Info")
     print( opel.info() )
     opel.to_csv('IntermediateDataPlots/opel.csv', index=False)
@@ -115,18 +118,19 @@ print( opel.isna().sum() )
 # Concatenate both opel and peugeot
 merged_dataset = pd.concat([peugeot, opel])
 
-if visualise == 1:
+if verbose == 1:
     print("\n\nPrinting Opel and Peugeot Merged Info")
     print( merged_dataset.info() )
     sliced_df = merged_dataset.head(5)
     sliced_df = pd.concat([sliced_df, merged_dataset.tail(5)], ignore_index=True)
     print (sliced_df)
-    merged_dataset.to_csv('IntermediateDataPlots/merged.csv', index=False)
+    if save_intermediate_files == 1:
+        merged_dataset.to_csv('IntermediateDataPlots/merged.csv', index=False)
 
 # We only care about predicting driving style here, so drop the roadSurface and
 # traffic columns, can also drop Unnamed
 trimmed_dataset = merged_dataset.drop(columns=['Unnamed: 0', 'roadSurface', 'traffic', 'brand'])
-if visualise == 1:
+if verbose == 1:
     print("\n\nPrinting Trimmed Merged Dataset Info")
     print( trimmed_dataset.info() )
 
@@ -150,28 +154,30 @@ Will want to return to this, there are a number of features which are highly
 correlated so can experiment with dropping some of them.
 '''
 
+feature_selected_dataset = trimmed_dataset.drop(columns=['MassAirFlow', 'VehicleSpeedInstantaneous'])
+
 ###################################################################################
 # Label encoding driving style and brand
 ###################################################################################
 
 # Need to encode the drivingSyle into a numeric value
 
-driving_style = trimmed_dataset['drivingStyle']
-X = trimmed_dataset.drop(columns=['drivingStyle'])
+driving_style = feature_selected_dataset['drivingStyle']
+X = feature_selected_dataset.drop(columns=['drivingStyle'])
 
 encoder = LabelEncoder()
 encoded_ds = encoder.fit_transform(driving_style)
 # Convert the array to a DataFrame
 y = pd.DataFrame(encoded_ds, columns=['encodedDrivingStyle'])
-if visualise == 1:
+if verbose == 1:
     print("\n\nLabel encoded dataset")
     print( y.info() )
 
-y.to_csv('IntermediateDataPlots/y.csv', index=False)
+if save_intermediate_files == 1:
+    y.to_csv('IntermediateDataPlots/y.csv', index=False)
+    X.to_csv('IntermediateDataPlots/X.csv', index=False)
 
-X.to_csv('IntermediateDataPlots/X.csv', index=False)
-
-if visualise == 1:
+if verbose == 1:
     print("\n\nDataset after encoding drivingStyle")
     print( X.info() )
 
@@ -186,24 +192,31 @@ X_scaled = scaler.fit_transform(X)
 # Split the data between testing and training
 X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3, random_state=42)
 
-X_train_df = pd.DataFrame(X_train)
-X_train_df.to_csv('IntermediateDataPlots/X_train.csv')
+if save_intermediate_files == 1:
+    X_train_df = pd.DataFrame(X_train)
+    X_train_df.to_csv('IntermediateDataPlots/X_train.csv')
 
 ###################################################################################
 # Addressing class imbalance
 ###################################################################################
 
-value_driving_peugeot = peugeot['drivingStyle'].value_counts()
-print('\n\nPrinting the count of driving style values from the Peugeot dataset')
-print(value_driving_peugeot)
+if verbose == 1:
+    value_driving_peugeot = peugeot['drivingStyle'].value_counts()
+    print('\n\nPrinting the count of driving style values from the Peugeot dataset')
+    print(value_driving_peugeot)
 
-value_driving_opel = opel['drivingStyle'].value_counts()
-print('\n\nPrinting the count of driving style values from the Opel dataset')
-print(value_driving_opel)
+    value_driving_opel = opel['drivingStyle'].value_counts()
+    print('\n\nPrinting the count of driving style values from the Opel dataset')
+    print(value_driving_opel)
 
-value_driving_y = y.value_counts()
-print('\n\nPrinting the count of driving style values from the merged dataset')
-print(value_driving_y)
+    value_driving_y = y.value_counts()
+    print('\n\nPrinting the count of driving style values from the merged dataset')
+    print(value_driving_y)
+    
+    n_agressive_samples = sum(y_train.encodedDrivingStyle == 0)
+    n_evenpace_samples = sum(y_train.encodedDrivingStyle == 1)
+    print( "\n\nNumber of Agressive driving style samples: ", n_agressive_samples )
+    print( "Number of EvenPace driving style samples: ", n_evenpace_samples )
 
 '''
 Peugeot: EvenPaceStyle 11962, AggressiveStyle 670. Minority class outnumbered almost 18:1
@@ -213,45 +226,53 @@ There is no formal definiion of imbalance, but I suspect we do have an imbalance
 at least with the Peugeot data.
 '''
 
-# Undersampling
-
-n_agressive_samples = sum(y_train.encodedDrivingStyle == 0) # 1901 = 0 = Agressive
-n_evenpace_samples = sum(y_train.encodedDrivingStyle == 1) # 14732 = 1 = EvenPace
-print( "\n\nNumber of Agressive driving style samples: ", n_agressive_samples )
-print( "Number of EvenPace driving style samples: ", n_evenpace_samples )
-undersample = RandomUnderSampler(sampling_strategy=0.5)
-X_train_under, y_train_under = undersample.fit_resample(X_train, y_train)
-
-print("\n\nShape of X_train_under: ", np.shape(X_train_under) )
-print( "Shape of y_train_under: ", np.shape(y_train_under) )
-n_agressive_samples_under = sum(y_train_under.encodedDrivingStyle == 0)
-n_evenpace_samples_under = sum(y_train_under.encodedDrivingStyle == 1)
-print( "\n\nNumber of Agressive driving style samples undersampled: ", n_agressive_samples_under ) #1902
-print( "Number of EvenPace driving style samples undersampled: ", n_evenpace_samples_under ) #3802
-
-X_train_under_df = pd.DataFrame(X_train_under)
-X_train_under_df.to_csv('IntermediateDataPlots/X_train_under.csv')
-y_train_under_df = pd.DataFrame(y_train_under)
-y_train_under_df.to_csv('IntermediateDataPlots/y_train_under.csv')
-
 # SMOTE
+X_train_A = X_train
+y_train_A = y_train
+if use_smote == 1:
+    smote = SMOTE(random_state=68, sampling_strategy=0.25)
+    X_train_smote, y_train_smote = smote.fit_resample( X_train, y_train )
+    X_train_A = X_train_smote
+    y_train_A = y_train_smote
+    if verbose == 1:
+        print("\n\nShape of X_train_smote: ", np.shape(X_train_smote) )
+        print( "Shape of y_train_smote: ", np.shape(y_train_smote) )
+        n_agressive_samples_smote = sum(y_train_smote.encodedDrivingStyle == 0)
+        n_evenpace_samples_smote = sum(y_train_smote.encodedDrivingStyle == 1)
+        print( "\n\nNumber of Agressive driving style samples smote: ", n_agressive_samples_smote )
+        print( "Number of EvenPace driving style samples smote: ", n_evenpace_samples_smote )
 
-smote = SMOTE(random_state=42)
-X_train_smote, y_train_smote = smote.fit_resample( X_train_under, y_train_under )
+# Undersampling
+X_train_B = X_train_A
+y_train_B = y_train_A
+if use_under_sampling == 1:
+    undersample = RandomUnderSampler(sampling_strategy=0.5)
+    X_train_under, y_train_under = undersample.fit_resample(X_train_A, y_train_A)
+    X_train_B = X_train_under
+    y_train_B = y_train_under
+    if verbose == 1:
+        print("\n\nShape of X_train_under: ", np.shape(X_train_under) )
+        print( "Shape of y_train_under: ", np.shape(y_train_under) )
+        n_agressive_samples_under = sum(y_train_under.encodedDrivingStyle == 0)
+        n_evenpace_samples_under = sum(y_train_under.encodedDrivingStyle == 1)
+        print( "\n\nNumber of Agressive driving style samples undersampled: ", n_agressive_samples_under )
+        print( "Number of EvenPace driving style samples undersampled: ", n_evenpace_samples_under )
 
-print("\n\nShape of X_train_smote: ", np.shape(X_train_smote) )
-print( "Shape of y_train_smote: ", np.shape(y_train_smote) )
-n_agressive_samples_smote = sum(y_train_smote.encodedDrivingStyle == 0)
-n_evenpace_samples_smote = sum(y_train_smote.encodedDrivingStyle == 1)
-print( "\n\nNumber of Agressive driving style samples undersampled: ", n_agressive_samples_smote ) #3802
-print( "Number of EvenPace driving style samples undersampled: ", n_evenpace_samples_smote ) #3802
+    if save_intermediate_files == 1:
+        X_train_under_df = pd.DataFrame(X_train_under)
+        X_train_under_df.to_csv('IntermediateDataPlots/X_train_under.csv')
+        y_train_under_df = pd.DataFrame(y_train_under)
+        y_train_under_df.to_csv('IntermediateDataPlots/y_train_under.csv')
 
 ###################################################################################
 # Logistic Regression
 ###################################################################################
 
-lgr_clf = LogisticRegression(max_iter=1000)
-lgr_clf.fit(X_train_smote, np.ravel(y_train_smote))
+X_train_final = X_train_B
+y_train_final = y_train_B
+
+lgr_clf = LogisticRegression(max_iter=10000)
+lgr_clf.fit(X_train_final, np.ravel(y_train_final))
 y_pred_lgr = lgr_clf.predict(X_test)
 
 print( "\n\nPRINT RESULTS FOR Logistic Regression" )
@@ -265,7 +286,7 @@ print( confusion_matrix( y_test, y_pred_lgr ) )
 ###################################################################################
 
 knn_clf = KNeighborsClassifier(n_neighbors=5)
-knn_clf.fit(X_train_smote, np.ravel(y_train_smote))
+knn_clf.fit(X_train_final, np.ravel(y_train_final))
 y_pred_knn = knn_clf.predict(X_test)
 
 print( "\n\nPRINT RESULTS FOR KNN" )
